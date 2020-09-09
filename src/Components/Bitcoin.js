4417b2239a3b2fd1647c1df71b4f8adf22d2f132
vuongvgc <vuongvgc@gmail.com>
2020-09-09 22:41:54 +0700

    add check fetch status and use throw to catch and add status loading

--- src/Components/Bitcoin.js
+++ src/Components/Bitcoin.js
@@ -6,24 +6,35 @@ class BitcoinPrice extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            loading: false,
             dataCoin: ''
         }
     }
     componentDidMount() {
+        this.setState({loading: true})
         fetch('https://blockchain.info/ticker')
-            .then(Response => Response.json())
-            .then(data => {
-                this.setState({dataCoin: data.USD})
+            .then(Response => {
+                if(Response.ok){
+                   return Response.json()
+                }else {
+                    throw new Error("NETWORK RESPONSE ERROR")
+                }
             })
-            .catch(Error => {
-                console.log(`some ${Error}`)
+            .then(data => {
+                this.setState({dataCoin: data.USD, loading:false})
+            })
+            .catch(error => {
+                this.setState({error, loading: true})
+                console.log(`some ${error}`)
             })
     }
     render() {
-        const dataCoin = this.state.dataCoin;
+        const {loading, dataCoin} = this.state;
+        const output = loading ? "Loading ....": "Successful"
         return(
             <div>
                 <img src={logoBitcoin} alt="Bitcoin logo"/>
+                <div>{output}</div>
                 <DetailItem dataItem = {dataCoin} />
             </div>
 
